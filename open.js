06b16544f66f26a965097a9e7d005dcf871ae318
DarkTrooper1 <146754265+DarkTrooper1@users.noreply.github.com>
2023-10-08 13:27:37 +0100

    Create open.js

---
+++ open.js
@@ -0,0 +1,17 @@
+function openWin() {
+  window.close();
+  var win = window.open();
+  var iframe = win.document.createElement('iframe');
+  win.document.write('<html><head><link href="style.css" rel="stylesheet" type="text/css" /><meta name="viewport" content="width=device-width"></head></html>')
+  iframe.style.width = "100%";
+  iframe.style.height = "100%";
+  iframe.style.border = "none";
+  iframe.frameBorder = 0;
+  iframe.src = "https://gooogle-classroom.vercel.app/home.html";
+  win.document.body.appendChild(iframe);
+};
+
+password = prompt("Please enter password: ")
+if (password == "darktrooper") {
+  openWin()
+}
