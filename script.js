eff2cceaadf6663ca0cf401388f77613991d73dd
DarkTrooper1 <146754265+DarkTrooper1@users.noreply.github.com>
2023-10-09 17:51:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -60,3 +60,30 @@ function changeConstructGame(game) {
 function escape() {
   window.open("https://classroom.google.com/");
 };
+
+function openWin() {
+  var win = window.open();
+  var iframe = win.document.createElement('iframe');
+  win.document.write('<html><head><link href="style.css" rel="stylesheet" type="text/css" /><meta name="viewport" content="width=device-width"></head></html>')
+  iframe.style.width = "100%";
+  iframe.style.height = "100%";
+  iframe.style.border = "none";
+  iframe.frameBorder = 0;
+  iframe.src = "https://gooogle-classroom.vercel.app/home.html";
+  win.document.body.appendChild(iframe);
+};
+
+function processForm() {
+  let password = document.getElementById('password').value;
+  if (password == "darktrooper") {
+    let form = document.getElementById('form');
+    form.remove();
+    window.location.replace("https://classroom.google.com/");
+    openWin();
+  } else {
+    alert('The password you entered is incorrect.');
+    let form = document.getElementById('form');
+    form.remove();
+    document.createElement('h1').value = "Incorrect password."
+  };
+};
