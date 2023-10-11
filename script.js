3d78ea0e4047c7bf0b9d3034004b0a00d1cbf9eb
DarkTrooper1 <146754265+DarkTrooper1@users.noreply.github.com>
2023-10-11 17:47:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ function processForm() {
     form.remove();
     openWin("https://gooogle-classroom.vercel.app/home.html");
   } else if (password == "classroom") {
-    top.window.opener.location.replace("https://classroom.google.com/");
+    window.location.href = "https://classroom.google.com/";
   } else {
     alert('The password you entered is incorrect.');
     let form = document.getElementById('form');
