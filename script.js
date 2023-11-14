f2d803b5eab21dd40beab240bd983328823b425c
DarkTrooper1 <146754265+DarkTrooper1@users.noreply.github.com>
2023-11-14 21:03:51 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 var count = 0;
 let index = 0; 
 
-function typingAnimation (id, text) {
+// function typingAnimation (id, text) {
 
-  setTimeout(() => {
-    index++;
-    updateText(id);
-    if (index < text.length) {
-      typingAnimation("title", game);
-    }
-  }, 200)
-}
+//   setTimeout(() => {
+//     index++;
+//     updateText(id);
+//     if (index < text.length) {
+//       typingAnimation("title", game);
+//     }
+//   }, 200)
+// }
 
-function updateText(id) {
-  const typing = document.getElementById(id); 
-  const lines = text.substring(0, index).split("<br>"); 
-  if(typing == null){  
-    return  
-  }
-  typing.innerHTML = lines.map((line, index) => {  
-    const isLastLine = index === lines.length - 1;
-    const lineBreak = isLastLine ? "" : "<br>";
-    return `${line}${lineBreak}`;  
+// function updateText(id) {
+//   const typing = document.getElementById(id); 
+//   const lines = text.substring(0, index).split("<br>"); 
+//   if(typing == null){  
+//     return  
+//   }
+//   typing.innerHTML = lines.map((line, index) => {  
+//     const isLastLine = index === lines.length - 1;
+//     const lineBreak = isLastLine ? "" : "<br>";
+//     return `${line}${lineBreak}`;  
 
-  }).join(""); 
-  }  
-}
+//   }).join(""); 
+//   }  
+// }
 
 function scratchWeb() {
   let now = new Date();
@@ -52,7 +52,8 @@ function changeScratchGame(game) {
   let scratchGameDict = {"Appel": "60917032", "Arena": "139025103", "14-15": "306576111", "Infinite Bunner": "567535145", "Desert Appel": "489697593", "Getting Over It": "389464290", "Heat Appel": "511913555", "Meltdown": "143663274", "Minecraft Appel": "678316153", "Moon Gravity": "423313626", "Massive Multiplayer": "612229554", "Subzero": "216343253", "Volcano Appel": "472031225", "World": "224236914", "Space Appel": "574035335", "Bionic Blitz": "539747811", "9-13": "140185501"};
 
   document.getElementById("gameIframe").src = "https://".concat(scratchWeb(), scratchGameDict[game], "/embed");
-  typingAnimation("title", game)
+  // typingAnimation("title", game)
+  document.getElementById("title") = game;
   document.getElementById("introText").innerHTML = " ";
 
   let items = document.querySelectorAll('a.active');
@@ -81,7 +82,8 @@ function changeOtherGame(game) {
   };
   let otherGameDict = {"Bandit Rip": "https://bandit.rip/", "Garden Gnomes": "https://lh3.googleusercontent.com/pHc09EKAAgzJVDw8O6Qd79GNqmsWhl-Xnv5GVuxbMn_YKcnE0CKjj1zAt_gsOnyi6bysD9YE7q1_GH-R0gkNKH_vno3oWzvQBRr44RAr=s0", "Slope": "Slope/", "Paper io": "Paper io/", "Retro Bowl": "Retro bowl/", "Getaway Shootout": "Getaway Shootout/", "Rooftop Snipers": "Rooftop Snipers/", "Motox3m Winter": "Motox3m Winter/", "Motox3m": "Motox3m/", "Basketball Stars": "Basketball Stars/", "2048": "2048/", "Awesome Tanks 2": "Awesome Tanks 2/", "Death Run 3D": "Death Run 3D/", "Motox3m Spooky": "Motox3m Spooky/", "Motox3m Pool": "Motox3m Pool/", "1v1lol": "1v1lol/", "Learn To Fly": "Learn To Fly/", "Among Us": "Among Us/"};
   document.getElementById("gameIframe").src = otherGameDict[game];
-  typingAnimation("title", game)
+  // typingAnimation("title", game)
+  document.getElementById("title") = game;
   if (otherGameDict[game] === "Rooftop Snipers/" || otherGameDict[game] === "Getaway Shootout/") {
     document.getElementById("introText").innerHTML = "This game takes about 30 seconds to load. PLEASE PRESS WAIT when you are given the option: it will load very soon after that.";
   } else if (otherGameDict[game] === "1v1lol/") {
@@ -109,7 +111,8 @@ function changeConstructGame(game) {
   let constructGameDict = {"Neon Rocket": "15727", "Ultra Pixel Survive": "26420"};
   
   document.getElementById("gameIframe").src = "https://games.construct.net/".concat(constructGameDict[game], "/latest");
-  typingAnimation("title", game)
+  // typingAnimation("title", game)
+  document.getElementById("title") = game;
   document.getElementById("introText").innerHTML = "";
 
   let items = document.querySelectorAll('a.active');
