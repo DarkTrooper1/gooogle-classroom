7c51e776a013fdff772057537473d1fcc0739332
DarkTrooper1 <146754265+DarkTrooper1@users.noreply.github.com>
2023-12-03 22:46:58 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Portal code
+
 const decipher = salt => {
   const textToChars = text => text.split('').map(c => c.charCodeAt(0));
   const applySaltToChar = code => textToChars(salt).reduce((a,b) => a ^ b, code);
@@ -10,10 +12,10 @@ const decipher = salt => {
 function openWin(link) {
   let win = window.open();
   let iframe = win.document.createElement('iframe');
-  win.document.write('<html><head><link href="style.css" rel="stylesheet" type="text/css" /><meta name="viewport" content="width=device-width"></head></html>')
-  iframe.style.width = "100%";
-  iframe.style.height = "100%";
-  iframe.style.border = "none";
+  win.document.write("<html><head><link href='style.css' rel='stylesheet' type='text/css' /><meta name='viewport' content='width=device-width'></head></html>")
+  iframe.style.width = '100%';
+  iframe.style.height = '100%';
+  iframe.style.border = 'none';
   iframe.frameBorder = 0;
   iframe.src = link;
   win.document.body.appendChild(iframe);
@@ -24,25 +26,48 @@ function processForm() {
   const imperialP = decipher('gg2934234g3429ed7820');
   const kgamesP = decipher('o293gfg2o39f8g23784v5i')
   let password = document.getElementById('password').value;
-  if (password === defaultP("6461726b74726f6f706572")) {
-    let form = document.getElementById('form');
+  if (password === defaultP('6461726b74726f6f706572')) {
+    let form = document.getElementById('passwordForm');
     form.remove();
-    openWin("https://improved-gooogle-classroom.vercel.app/home.html");
-    top.window.opener.location.replace("https://classroom.google.com/");
-  } else if (password === kgamesP("27203a3d28232e3c3c")) {
-    window.open("https://project-hourglass.vercel.app")
-    top.window.opener.location.replace("https://classroom.google.com/");
-  } else if (password === escapeP("525d504242435e5e5c")) {
-    window.open("https://classroom.google.com/");
-    top.window.opener.location.replace("https://classroom.google.com/");
-  } else if (password === imperialP("27312b2a2b3c3b")) {
-    let f = document.getElementById('form');
+    openWin('https://improved-gooogle-classroom.vercel.app/home.html');
+  } else if (password === kgamesP('27203a3d28232e3c3c')) {
+    window.open('https://project-hourglass.vercel.app')
+  } else if (password === escapeP('525d504242435e5e5c')) {
+    window.open('https://classroom.google.com/');
+  } else if (password === imperialP('27312b2a2b3c3b')) {
+    let f = document.getElementById('passwordForm');
     f.remove();
-    openWin("https://improved-gooogle-classroom.vercel.app/tube.html");
-    top.window.opener.location.replace("https://classroom.google.com/");
+    openWin('https://improved-gooogle-classroom.vercel.app/tube.html');
   } else {
     alert('The password you entered is incorrect.');
-    let form = document.getElementById('form');
+    let form = document.getElementById('passwordForm');
     form.remove();
   };
 };
+
+// Home Code
+
+let count = 0;
+
+function changeGame(game) {
+  count += 1;
+  if (count === 1) {
+    top.window.opener.location.replace('https://classroom.google.com/');
+  }
+  let title = document.getElementById('title');
+  let gameDict = {'Drive Mad': 'Drive Mad/'};
+  let gameIframe = document.getElementById('gameIframe');
+  let introText = document.getElementById('introText');
+  
+  title.innerHTML = game;
+  introText.style.display = 'none';
+  gameIframe.src = gameDict[game];
+  gameIframe.style.display = "block";
+  
+  let items = document.querySelectorAll('a.active');
+  for (let elem of items) {
+    elem.classList.remove('active');
+  };
+  let element = document.getElementById(game);
+  element.classList.add('active');
+}
